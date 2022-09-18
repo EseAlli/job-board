bcf3756d921d74634c1b393f321144eba2222db8
EseAlli <essealli.ea@gmail.com>
2022-09-18 11:30:25 +0100

    ✨ feature: navbar component created

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex shadow-md py-4 px-8 h-15 sticky">
+      <h1 className="text-[#2C80FF] text-xl font-semibold">Job Board</h1>
+    </div>
+  );
+};
+
+export default Navbar;
