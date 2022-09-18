bd178be3f4323b16d03e8a7da15e83471ea93d2a
EseAlli <essealli.ea@gmail.com>
2022-09-18 15:38:50 +0100

    ✨ feature: styled navbar textt responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,10 @@ import styles from "../../styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div className="flex shadow-md py-4 px-8 h-15 sticky">
-      <h1 className={`${styles.name} text-xl font-semibold lowercase`}>Jobpod</h1>
+    <div className="flex shadow-md py-4 px-8 h-15 sticky justify-start content-start">
+      <h1 className={`${styles.name} text-xl font-semibold lowercase md:px-5`}>
+        Jobpod
+      </h1>
     </div>
   );
 };
