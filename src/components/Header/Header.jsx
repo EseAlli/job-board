aaecf8bc568ee4186ce5df8d2c156fa12d01e8a5
EseAlli <essealli.ea@gmail.com>
2022-09-18 14:33:35 +0100

    ✨ feature: job component styled and added search component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,11 @@ import styles from "../../styles/Header.module.css";
 
 const Header = () => {
   return (
-    <div className="container mx-auto">
-      <div className="flex sm:flex-wrap lg:flex-nowrap w-full">
-        <div className={styles.image_wrapper}>
+    <div className="container mx-auto my-12 md:my-4">
+      <div className="flex flex-col md:flex-row sm:flex-wrap lg:flex-nowrap w-full">
+        <div
+          className={`${styles.image_wrapper} flex flex-wrap justify-center md:justify-between order-2 md:order-1 w-full lg:w-auto`}
+        >
           <Image
             src="https://joeschmoe.io/api/v1/jess"
             width={300}
@@ -18,16 +20,18 @@ const Header = () => {
             height={350}
           />
         </div>
-        <div className="flex flex-col items-center justify-center text-center mx-auto">
-          <h2 className="text-7xl font-black uppercase my-2">
+        <div className="flex flex-col items-center justify-center text-center mx-auto order-1 md:order-2">
+          <h2 className="text-4xl p-4 md:p-1 lg:text-7xl font-black uppercase md:my-2">
             Find Your Dream Job
           </h2>
-          <p className="text-xl font-semibold">
+          <p className="text-lg md:text-xl font-semibold px-2 md:p-1">
             Make the best move and find your next job
           </p>
           {/* <p className="text-xl font-medium">1500 Jobs Available</p> */}
         </div>
-        <div className={styles.image_wrapper2}>
+        <div
+          className={`${styles.image_wrapper2} hidden md:flex order-3 flex-wrap justify-between w-full lg:w-auto content-center items-center`}
+        >
           <Image
             src="https://joeschmoe.io/api/v1/jon"
             width={300}
