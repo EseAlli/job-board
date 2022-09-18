2f7762f732006dd4577787cce37ff097b6534520
EseAlli <essealli.ea@gmail.com>
2022-09-18 13:18:08 +0100

    ✨ feature: job component styled

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,30 +1,42 @@
 import React from "react";
 import Image from "next/image";
+import {
+  BriefcaseOutline,
+  LocationOutline,
+  StatsChartOutline,
+} from "react-ionicons";
 
 const Job = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <Image src="https://joeschmoe.io/api/v1/joe" width={30} height={50} />
+    <div className="flex rounded-lg bg-white justify-between items-center shadow-md p-3 py-4">
+      <div className="flex gap-5">
+        <div className="flex items-center rounded-full justify-center">
+          <Image src="https://joeschmoe.io/api/v1/joe" width={60} height={60} />
         </div>
-        <div>
-          <p>Spotify</p>
-          <p>Junior Frontend Developer</p>
-          <div>
-            <div>
-              <p>Part-Time</p>
+        <div className="flex flex-col gap-0 items-start">
+          <p className="text-base">Spotify</p>
+          <p className="font-bold text-lg">Junior Frontend Developer</p>
+          <div className="flex flex-col">
+            <div className="flex items-baseline gap-3">
+              <BriefcaseOutline color={"#00000"} height="14px" width="14px" />
+              <p className="text-sm">Part-Time</p>
             </div>
-            <div>
-              <p>Remote</p>
+            <div className="flex items-baseline gap-3">
+              <LocationOutline color={"#00000"} height="14px" width="14px" />
+              <p className="text-sm">Remote</p>
             </div>
-            <div>
-              <p>MidLevel</p>
+            <div className="flex items-baseline gap-3">
+              <StatsChartOutline color={"#00000"} height="14px" width="16px" />
+              <p className="text-sm">MidLevel</p>
             </div>
           </div>
         </div>
       </div>
-      <a>Apply</a>
+      <div className="flex self-end">
+        <a className="shadom-sm border border-[#2C80FF] rounded-full py-1 px-4 uppercase text-sm text-[#2C80FF] hover:cursor-pointer hover:bg-[#2C80FF] hover:text-white ease-in duration-300 tracking-tight font-semibold">
+          Apply
+        </a>
+      </div>
     </div>
   );
 };
--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -3,14 +3,29 @@ import Job from "../Job/Job";
 
 const JobList = () => {
   return (
-    <>
-      <div className="grid grid-cols-3 container mx-auto">
+    <div className="container mx-auto p-4 my-8">
+      <div className="flex items-center content-center justify-center">
+        <p className="text-2xl font-medium">1500 Jobs Available</p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 container mx-auto my-4">
+        <Job />
+        <Job />
+        <Job />
+        <Job />
+        <Job />
+        <Job />
+        <Job />
+        <Job />
+        <Job />
+        <Job />
+        <Job />
+        <Job />
         <Job />
         <Job />
         <Job />
         <Job />
       </div>
-    </>
+    </div>
   );
 };
 
