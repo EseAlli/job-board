95569c390708e9073a400da6d666a059ea72be34
EseAlli <essealli.ea@gmail.com>
2022-09-18 12:23:55 +0100

    ✨ feature: change title in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex shadow-md py-4 px-8 h-15 sticky">
-      <h1 className="text-xl font-semibold uppercase">Jobpod</h1>
+      <h1 className="text-xl font-semibold lowercase">Jobpod</h1>
     </div>
   );
 };
